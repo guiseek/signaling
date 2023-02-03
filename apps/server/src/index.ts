d10536061a6d8c3806a054b8f734883b5cf4f248
Gui Seek <guiseek@gmail.com>
2023-02-03 19:27:55 -0300

    chore: logs

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,7 +5,11 @@ const wss = new WebSocketServer({ port: 3000 })
 wss.on('connection', (ws) => {
   ws.on('message', (data) => {
     wss.clients.forEach((client) => {
+      console.log(client.readyState);
       if (client.readyState === 1) {
+        console.log(client.readyState);
+        console.log(data.toString());
+        
         client.send(data.toString())
       }
     })
